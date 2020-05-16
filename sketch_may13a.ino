33dd1ffbceaa4d92669d09653341423742e48a59
benbarnett02 <46703294+benbarnett02@users.noreply.github.com>
2020-05-16 10:57:33 +1000

    Add files via upload

---
+++ sketch_may13a.ino
@@ -0,0 +1,37 @@
+/*
+ * Notes
+ * Etc
+*/
+
+const int estopButtonPin = 2;// the number of the pushbutton pin
+const int liveButtonPin = 3;
+const int standbyButtonPin = 4;
+const int ButtonPin1 = 5;
+const int ButtonPin2 = 6;
+const int ButtonPin3 = 7;
+
+const int LiveLightPin = 8;// the number of the LED pin
+const int StandByLightPin = 9;
+const int CueLightPin1 = 10;
+const int Cuelightpin2 = 11;
+const int Cuelightpin3 = 12;
+
+boolean cue1standby = false
+boolean cue2standby = false
+boolean cue3standby = false
+
+boolean cue1go = false;
+boolean cue2go = false;
+boolean cue3go = false;
+
+unsigned long 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
