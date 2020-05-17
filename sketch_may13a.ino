ac729d950613f8ec991e584546dda4f776dfc7f1
benbarnett02 <46703294+benbarnett02@users.noreply.github.com>
2020-05-17 14:44:54 +1000

    Updated with if statements; deleted a few bits

--- sketch_may13a.ino
+++ sketch_may13a.ino
@@ -1,9 +1,13 @@
 /*
- * Notes
- * Cuelight
- * Etc
- * I know I could use arrays but I don't really understand them very well. 
- * The program isn't massive anyway. 
+   Notes
+   Cuelight
+   Etc
+   I know I could use arrays but I don't really understand them very well.
+   The program isn't massive anyway.
+   I also know I could use the IO register to remove a bunch of these vars. But I didn't.
+
+   Still need to implement the debouncing, I don't really want another thousand unsigned longs.
+   Could use ezbutton library or similar but Matt was having some issues with it
 */
 
 const int estopButtonPin = 2;// the number of the pushbutton pin
@@ -36,6 +40,8 @@ const int CueLightPin2 = 11;
 const int CueLightPin3 = 12;
 
 boolean LiveLight = false;
+boolean StandbyLight = false;
+
 boolean cue1standby = false;
 boolean cue2standby = false;
 boolean cue3standby = false;
@@ -50,21 +56,25 @@ unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 
 void setup() {
+  
   // put your setup code here, to run once:
   //was intending to use internal INPUT_PULLUP but unsure of using it with multiple pins
 
-// setting pinmodes of all buttons  
-pinMode(estopButtonPin, INPUT);
-pinMode(liveButtonPin, INPUT);
-pinMode(ButtonPin1, INPUT);
-pinMode(ButtonPin2, INPUT);
-pinMode(ButtonPin3, INPUT);
+  Serial.begin(9600);//making a serial thing
 
-pinMode(LiveLightPin, OUTPUT);
-pinMode(StandByLightPin, OUTPUT);
-pinMode(CueLightPin1, OUTPUT);
-pinMode(CueLightPin2, OUTPUT);
-pinMode(CueLightPin3, OUTPUT);
+  // setting pinmodes of all buttons
+  pinMode(estopButtonPin, INPUT);
+  pinMode(liveButtonPin, INPUT);
+  pinMode(ButtonPin1, INPUT);
+  pinMode(ButtonPin2, INPUT);
+  pinMode(ButtonPin3, INPUT);
+
+  //pinmodes all lights
+  pinMode(LiveLightPin, OUTPUT);
+  pinMode(StandByLightPin, OUTPUT);
+  pinMode(CueLightPin1, OUTPUT);
+  pinMode(CueLightPin2, OUTPUT);
+  pinMode(CueLightPin3, OUTPUT);
 
 
 }
@@ -72,23 +82,57 @@ pinMode(CueLightPin3, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
   //Reading all the button states and writing them to integers (defined in top level)
-estopButton = digitalRead(estopButtonPin);
-liveButton = digitalRead(liveButtonPin);
-liveButton = digitalRead(liveButtonPin  ); 
-Button1 = digitalRead(ButtonPin1); 
-Button2 = digitalRead(ButtonPin2); 
-Button3 = digitalRead(ButtonPin3); 
-  if (liveButton == HIGH && liveButtonLast == LOW) {
+  estopButton = digitalRead(estopButtonPin);
+  liveButton = digitalRead(liveButtonPin);
+  standbyButton = digitalRead(standbyButtonPin);
+  Button1 = digitalRead(ButtonPin1);
+  Button2 = digitalRead(ButtonPin2);
+  Button3 = digitalRead(ButtonPin3);
 
-    if (LiveLight == 0) {
-      // Toggle on
-      digitalWrite(LiveLightPin, HIGH);
-      LiveLight = 1;
+  //Cue 1 ifs
+  if (Button1 == HIGH && Button1Last == LOW) {//If button is pushed down
 
-    } else {
-      // Toggle off
-      digitalWrite(LiveLightPin, LOW);
-      LiveLight = 0;
+    if (cue1standby == 0) {//if button is pushed AND the LED is currently off
+      // turn it on
+      digitalWrite(CueLightPin1, HIGH);
+      cue1standby = 1;
+
+    } else {//if button is pushed and the LED is anything but off
+      // turn it off
+      digitalWrite(CueLightPin1, LOW);
+      cue1standby = 0;
+    }  
+  }
+
+  //Cue light 2 ifs
+    if (Button2 == HIGH && Button2Last == LOW) {//If button is pushed down
+
+    if (cue2standby == 0) {//if button is pushed AND the LED is currently off
+      // turn it on
+      digitalWrite(CueLightPin2, HIGH);
+      cue2standby = 1;
+
+    } else {//if button is pushed and the LED is anything but off
+      // turn it off
+      digitalWrite(CueLightPin2, LOW);
+      cue2standby = 0;
     }
   }
+
+  //Cue light 3 ifs
+    if (Button3 == HIGH && Button3Last == LOW) {//If button is pushed down
+
+    if (cue3standby == 0) {//if button is pushed AND the LED is currently off
+      // turn it on
+      digitalWrite(CueLightPin3, HIGH);
+      cue3standby = 1;
+
+    } else {//if button is pushed and the LED is anything but off
+      // turn it off
+      digitalWrite(CueLightPin3, LOW);
+      cue3standby = 0;
+    }
+  }
+  
+
 }
